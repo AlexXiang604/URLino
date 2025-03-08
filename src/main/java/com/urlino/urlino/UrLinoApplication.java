3072784e4ae53b4ac3be906fa99e96799484ddfb
AlexXiang604 <qx501@nyu.edu>
2025-03-08 14:55:34 -0600

    Initial commit

---
+++ src/main/java/com/urlino/urlino/UrLinoApplication.java
@@ -0,0 +1,13 @@
+package com.urlino.urlino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrLinoApplication.class, args);
+    }
+
+}
