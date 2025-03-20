0a4b65def30a7f5ca4b5d571a8389ba9864f413b
AlexXiang604 <qx501@nyu.edu>
2025-03-20 17:21:58 -0500

    update controller - TBD

--- src/main/java/com/urlino/urlino/controller/UrlShortenerController.java
+++ src/main/java/com/urlino/urlino/controller/UrlShortenerController.java
@@ -1,4 +1,31 @@
 package com.urlino.urlino.controller;
 
+import com.urlino.urlino.model.UrlMapping;
+import com.urlino.urlino.service.UrlShortenerService;
+import com.urlino.urlino.repository.UrlMappingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/urlino")
 public class UrlShortenerController {
+
+    @Autowired
+    private UrlShortenerService Service;
+    private UrlMappingRepository Repository;
+
+    @RequestMapping(value = "/{url}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity shortenUrl(@PathVariable String url) {
+        String shortUrlEntry = Service.shortenUrl(url);
+        return ResponseEntity.ok(url);
+    }
+
+    @RequestMapping(value = "/{key}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity getUrl(@PathVariable String key) {
+        String url = Repository.findLongUrlById(key);
+        return ResponseEntity.ok(url);
+    }
 }
